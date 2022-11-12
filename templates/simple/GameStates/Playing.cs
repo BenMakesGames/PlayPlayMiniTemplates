5988d7603c2c30c609d4e2243d8a1462fd5858a4
Ben H <ben@benmakesgames.com>
2022-11-12 11:29:55 -0500

    playplay mini update + adding a template that demonstrates more features

---
+++ templates/simple/GameStates/Playing.cs
@@ -0,0 +1,55 @@
+using BenMakesGames.PlayPlayMini;
+using BenMakesGames.PlayPlayMini.Services;
+using Microsoft.Xna.Framework;
+
+namespace MyNamespace.GameStates;
+
+// sealed classes execute faster than non-sealed, so always seal your game states!
+public sealed class Playing: GameState
+{
+    private GraphicsManager Graphics { get; }
+    private KeyboardManager Keyboard { get; }
+    private GameStateManager GSM { get; }
+
+    public Playing(GraphicsManager graphics, GameStateManager gsm, KeyboardManager keyboard)
+    {
+        Graphics = graphics;
+        GSM = gsm;
+        Keyboard = keyboard;
+    }
+    
+    // overriding lifecycle methods is optional; feel free to delete any overrides you're not using.
+    // note: you do NOT need to call the `base.` for lifecycle methods. so save some CPU cycles,
+    // and don't call them :P
+
+    public override void ActiveInput(GameTime gameTime)
+    {
+        // TODO: get input from keyboard (refer to PlayPlayMini documentation for more info)
+    }
+
+    public override void ActiveUpdate(GameTime gameTime)
+    {
+        // TODO: update game objects based on user input, AI logic, etc
+    }
+
+    public override void AlwaysUpdate(GameTime gameTime)
+    {
+    }
+
+    public override void ActiveDraw(GameTime gameTime)
+    {
+    }
+
+    public override void AlwaysDraw(GameTime gameTime)
+    {
+        // TODO: draw game scene (refer to PlayPlayMini documentation for more info)
+    }
+
+    public override void Enter()
+    {
+    }
+
+    public override void Leave()
+    {
+    }
+}
---
+++ templates/simple/Program.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using BenMakesGames.PlayPlayMini;
+using BenMakesGames.PlayPlayMini.Model;
+using MyNamespace.GameStates;
+
+// TODO: any pre-req setup, ex:
+/*
+ * var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+ * var appDataGameDirectory = @$"{appData}{Path.DirectorySeparatorChar}MyNamespace";
+ * 
+ * Directory.CreateDirectory(appDataGameDirectory);
+ */
+
+var gsmBuilder = new GameStateManagerBuilder();
+
+gsmBuilder
+    .SetWindowSize(1920 / 4, 1080 / 4, 2)
+    .SetInitialGameState<Startup>()
+    
+    // TODO: set a better window title
+    .SetWindowTitle("MyNamespace")
+    
+    // TODO: add any resources needed (refer to PlayPlayMini documentation for more info)
+    .AddFonts(new FontMeta[] {
+        
+    })
+    .AddPictures(new PictureMeta[] {
+        
+    })
+    .AddSpriteSheets(new SpriteSheetMeta[] {
+        
+    })
+    .AddSongs(new SongMeta[] {
+        
+    })
+    .AddSoundEffects(new SoundEffectMeta[] {
+        
+    })
+    
+    // TODO: any additional service registration (refer to PlayPlayMini and/or Autofac documentation for more info)
+    .AddServices(s => {
+        
+    })
+;
+
+gsmBuilder.Run();
