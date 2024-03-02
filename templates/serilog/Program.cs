2c7d1815f496f4989cb7a8b427553bfc568d6f37
Ben H <ben@benmakesgames.com>
2024-03-02 09:41:55 -0500

    2.1.0 - package upgrades - PlayPlayMini 4.4.0, and Serilog 3.1.1 + safer Serilog config defaults

--- templates/serilog/Program.cs
+++ templates/serilog/Program.cs
@@ -35,9 +35,10 @@ gsmBuilder
 
     // TODO: any additional service registration (refer to PlayPlayMini and/or Autofac documentation for more info)
     .AddServices((s, c) => {
-        var logPath = $"{appDataGameDirectory}{Path.DirectorySeparatorChar}Log.log";
+        var loggerConfig = new LoggerConfiguration()
+            .WriteTo.File($"{appDataGameDirectory}{Path.DirectorySeparatorChar}Log.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7);
 
-        s.RegisterSerilog(logPath);
+        s.RegisterSerilog(loggerConfig);
     })
 ;
 
